f6369f2ea6ff11a874bff236764c1b93a8b8ed37
mhkb2216 <146079322+mhkb2216@users.noreply.github.com>
2023-10-10 15:26:37 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,29 @@
 import java.util.Scanner;
 
-public class Main {
+public class BMICalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Prompt the user for weight in kilograms
         System.out.print("Enter your weight in kilograms: ");
         double weight = input.nextDouble();
 
-        // Prompt the user for height in meters
         System.out.print("Enter your height in meters: ");
         double height = input.nextDouble();
 
-        // Calculate BMI
         double bmi = calculateBMI(weight, height);
 
-        // Determine BMI category
         String category = interpretBMI(bmi);
 
-        // Display the result
         System.out.printf("Your BMI is: %.2f%n", bmi);
         System.out.println("BMI Category: " + category);
 
         input.close();
     }
 
-    ///this is first commit
-    // Function to calculate BMI
     public static double calculateBMI(double weight, double height) {
         return weight / (height * height);
     }
 
-    // Function to interpret BMI category
     public static String interpretBMI(double bmi) {
         if (bmi < 18.5) {
             return "Underweight";
